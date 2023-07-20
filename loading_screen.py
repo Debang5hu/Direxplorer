1b6e315f9dff7462cf9164ef7f9e53383934f34f
Debangshu <114200360+Debang5hu@users.noreply.github.com>
2023-07-20 09:07:51 +0530

    Add files via upload

---
+++ loading_screen.py
@@ -0,0 +1,22 @@
+def loading_screen():    
+        print('''  
+  ___________
+| Hello World |
+  ===========
+                \\
+                 \\
+                  \\
+                   .--.
+                  |o_o |
+                  |:_/ |
+                 //   \ \\
+                (|     | )
+               /'\_   _/`\\
+               \___)=(___/
+      
+      \n\t\t\t\t\tcreated by: Debangshu Roy
+      \n\t\t\t\tWeb Directory Scanner made in Python
+''')
+
+'''else:
+                        print("\033[0m", "/" + x + "  " + "-> "+ "status: " + str(response.status_code) )'''
