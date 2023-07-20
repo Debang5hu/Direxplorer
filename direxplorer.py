f7a929732977f0950f43227a6d4474a7fcec8237
Debangshu <114200360+Debang5hu@users.noreply.github.com>
2023-07-20 09:07:19 +0530

    Update direxplorer.py

--- direxplorer.py
+++ direxplorer.py
@@ -2,29 +2,7 @@ import requests
 import platform
 import os
 from time import sleep
-
-def loading_screen():    
-        print("""           /^\/^\                                  """)                                                            
-        print("""         _|__|  O|                                 """)                 
-        print("""\/     /~     \_/ \                                """)
-        print(""" \____|__________/  \                              """)
-        print("""        \_______      \                            """)
-        print("""                `\     \                 \         """)
-        print("""                  |     |                  \       """)
-        print("""                 /      /                    \     """)
-        print("""                /     /                       \\   """)
-        print("""              /      /                         \ \ """)
-        print("""             /     /                            \  \ """)
-        print("""           /     /             _----_            \   \ """)
-        print("""          /     /           _-~      ~-_         |   |""")
-        print("""         (      (        _-~    _--_    ~-_     _/   |""")
-        print("""          \      ~-____-~    _-~    ~-_    ~-_-~    / """)
-        print("""            ~-_           _-~          ~-_       _-~  """)
-        print("""               ~--______-~                ~-___-~     """)
-        print("\n\t\t\t\t\tcreated by: Debangshu Roy")
-        print("\n\t\t\t\tA simple Web Directory Scanner made in Python")
-        print("--------------------------------------------------------------------------------")
-        print("\n")
+from loading_screen import loading_screen
 
 
 def clearscreen():
@@ -38,19 +16,22 @@ def directory_search(address,wordlist):
     with open (wordlist,'r') as fh:
         newaddress=fh.read()
         for x in newaddress.split("\n"):   
-            dir=(address + "/" + x)
-            response=requests.get("{}".format(dir))
-            if(response.status_code==200):
-                print("\033[0;32m", "/" + x + "  " + "-> "+ "status: " + str(response.status_code) )
-            else:
-                print("\033[0m", "/" + x + "  " + "-> "+ "status: " + str(response.status_code) )
+            try:
+                dir=(address + "/" + x)
+                response=requests.get("{}".format(dir))
+                if(response.status_code==200) or (response.status_code==301):
+                        print("\033[0;32m", "/" + x + "  " + "-> "+ "status: " + str(response.status_code) )
+            except:
+                 pass
 
 if __name__=="__main__":
+    print("\033[0m")
     clearscreen()
     loading_screen()
-    address=input("Enter the url: ")
-    wordlist=input("Enter the absolute path of wordlist: ")
-    print("\nTarget= {}\nWordlist= {}\n".format(address,wordlist))
-    print("Searching...")
+    address=input("[*] Enter the url(https://www.example.com): ")
+    wordlist=input("[*] Enter the absolute path of wordlist: ")
+    print("\n[#] Target= {}\n[#] Wordlist= {}\n".format(address,wordlist))
+    print("[*] Searching...\n")
     sleep(3)
     directory_search(address,wordlist)
+    print("\033[0m")
