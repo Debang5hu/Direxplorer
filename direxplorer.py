b9edc51614b4a9d178ea5937747b5602ade5a544
Debangshu <114200360+Debang5hu@users.noreply.github.com>
2023-08-06 02:01:37 +0530

    Update direxplorer.py

--- direxplorer.py
+++ direxplorer.py
@@ -1,6 +1,6 @@
 try:
     import platform
-    import os
+    import os,sys,getopt
     from loading_screen import loading_screen
     import asyncio
     import aiohttp
@@ -13,6 +13,7 @@ def clearscreen():
         os.system("cls")
     else:
         os.system("clear")
+
 
 async def fetch_response_status(session,url):
     try:
@@ -42,7 +43,9 @@ async def directory_search(address,wordlist):
     
     for x,y in zip(urllist,responses):
         if y in [200,301]:
-            print("\033[0;32m",x , " " + "-> " + "status: " , y ,"\033[00m")
+            print("\033[0;32m",x , " " + "-> " + "status:" , y ,"\033[00m")
+        if y in [403]:
+            print("\033[0;31m",x , " " + "-> " + "status:" , y ,"\033[00m")
     print('''
 ==========================================================================================
        DIRECTORY FUZZING COMPLETED
@@ -50,21 +53,35 @@ async def directory_search(address,wordlist):
 ''')
 
 
+
 if __name__=="__main__":
 
     clearscreen()
     loading_screen()
-    
-    address=input("\033[0;34m[*] Enter the url(https://www.example.com): \033[00m")
-    wordlist=input("\033[0;34m[*] Enter the absolute path of wordlist: \033[00m")
+
+    #argv
+    #target_url=sys.argv[1]
+    #target_wordlist=sys.argv[2]
+
+    arguments=sys.argv[1:]
+
+    try:
+        args,null=getopt.getopt(arguments,"u:w:",["url=","wordlist="])
+        for x,y in args:
+            if x in ['-u','--url']:
+                target_url=y
+            if x in ['-w','--wordlist']:
+                target_wordlist=y
+            
+    except:
+        print("\033[0;31m[#] Error Encounted!\033[00m")
     
     print('''
 ==========================================================================================
-    \n\033[0;34m[#] Target= \033[1;31m{}\033[0;34m\n[#] Wordlist= \033[1;31m{}\033[0;34m\n[*] Searching...\033[00m
+    \n\033[0;34m[#] Target= \033[1;31m{}\033[0;34m\n[#] Wordlist= \033[1;31m{}\033[0;34m\n\033[00m
 ==========================================================================================\n
-'''.format(address,wordlist))
+'''.format(target_url,target_wordlist))
     
     
     #directory_search(address,wordlist)
-    asyncio.run(directory_search(address,wordlist)) #asyncio
-    print("\033[0m")
+    asyncio.run(directory_search(target_url,target_wordlist)) #asyncio
