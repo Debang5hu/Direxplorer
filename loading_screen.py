228f57965d5b4ef43a8e16597a60109f6e72b26c
Debangshu <114200360+Debang5hu@users.noreply.github.com>
2023-10-27 13:44:18 +0530

    Update loading_screen.py

--- loading_screen.py
+++ loading_screen.py
@@ -1,21 +1,8 @@
-def loading_screen():    
-        
-          print('''  
-  __________________
-| Direxplorer v:0.1 |
-  ===========
-                \\
-                 \\
-                  \\
-                   .--.
-                  |o_o |
-                  |:_/ |
-                 //   \ \\
-                (|     | )
-               /'\_   _/`\\
-               \___)=(___/
-      
-      \n\t\t\t\t\tcreated by: \033[0;34mDebangshu Roy\033[00m
-      \n\t\t\t\t\033[0;34mWeb Directory Scanner\033[00m made in \033[0;34mPython\033[00m
+def loading_screen():
+    print('''
+ ____________________
+| Direxplorer v:0.1  |
+  ===================
+    \t\033[0;34mWeb Directory Scanner\033[00m made in \033[0;34mPython\033[00m : \033[0;34m@Debang5hu\033[00m
+
 ''')
-    
