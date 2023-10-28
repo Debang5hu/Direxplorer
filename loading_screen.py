84d5fb1786f4b6961d8eae330687f097d690523f
Debangshu <114200360+Debang5hu@users.noreply.github.com>
2023-10-28 15:49:32 +0530

    Delete loading_screen.py

--- loading_screen.py
+++
@@ -1,8 +0,0 @@
-def loading_screen():
-    print('''
- ____________________
-| Direxplorer v:0.1  |
-  ===================
-    \t\033[0;34mWeb Directory Scanner\033[00m made in \033[0;34mPython\033[00m : \033[0;34m@Debang5hu\033[00m
-
-''')
